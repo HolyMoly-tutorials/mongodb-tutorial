1edeee68db8f3c53e39fd6222f3c4d798ab7c24d
jun094 <jun094@naver.com>
2023-05-21 17:29:36 +0900

    feat(step2): api endpoint 생성

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/user', require('./routes/api/user'));
 
 const PORT = process.env.PORT || 5001;
 
