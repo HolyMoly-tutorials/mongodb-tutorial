95b2de604c29e700a7935ed3ff3d97edb5905312
jun094 <jun094@naver.com>
2023-05-21 17:09:53 +0900

    feat(step1): express, mongoose 세팅

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
+
+app.get('/', (req, res) => res.send('API running'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server starts on port ${PORT}`));
